b5c0341e2dcd1fe4966394109367b8ff698f8795
Tylor Pelletier <tylorpelletier@Tylors-MacBook-Pro.local>
2018-08-10 14:46:22 -0500

    finished tables exercise

---
+++ albums_migration.sql
@@ -0,0 +1,19 @@
+SHOW DATABASES;
+
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums(
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  artist VARCHAR(100) NOT NULL,
+  name VARCHAR (100) NOT NULL,
+  release_date DATE NOT NULL,
+  sales DOUBLE NOT NULL,
+  genre VARCHAR (50),
+  PRIMARY KEY (id)
+);
+
+SHOW TABLES;
+DESCRIBE albums;
+SHOW CREATE TABLE albums\G;
