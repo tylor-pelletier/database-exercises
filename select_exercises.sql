8d1731e530ac0aec20fe840f5ede918a98371934
Tylor Pelletier <tylorpelletier@Tylors-MacBook-Pro.local>
2018-08-13 11:03:06 -0500

    finished album seeder and select exercise

---
+++ select_exercises.sql
@@ -0,0 +1,25 @@
+USE codeup_test_db;
+
+SELECT name
+FROM albums
+WHERE artist = 'Pink Floyd';
+
+SELECT release_date
+FROM albums
+WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
+
+SELECT genre
+FROM albums
+WHERE name = 'Nevermind';
+
+SELECT *
+FROM albums
+WHERE release_date BETWEEN 1990 AND 1999;
+
+SELECT *
+FROM albums
+WHERE sales < 20.0;
+
+SELECT *
+FROM albums
+WHERE genre = 'Rock';
