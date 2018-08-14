4fca8772080a42cb587507ede2e3a26e33af360a
Tylor Pelletier <tylorpelletier@Tylors-MacBook-Pro.local>
2018-08-14 15:09:02 -0500

    added notes

--- aliases_exercises.sql
+++ aliases_exercises.sql
@@ -5,3 +5,9 @@ LIMIT 10;
 SELECT CONCAT(emp_no, ' -  ', last_name, ', ', first_name) AS 'full_name', birth_date AS DOB
 FROM employees
 LIMIT 10;
+
+-- Notes
+
+-- SELECT CONCAT_WS(' ', emp_no, first_name, last_name, hire_date) AS employee_work_info
+-- FROM employees
+-- LIMIT 50;
