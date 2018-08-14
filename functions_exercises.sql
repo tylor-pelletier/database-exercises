8cdfc73dfcfa04a307c5c7316fc52793e051173c
Tylor Pelletier <tylorpelletier@Tylors-MacBook-Pro.local>
2018-08-14 11:21:26 -0500

    added notes

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -13,3 +13,17 @@ SELECT CONCAT(first_name, ' ', last_name, ' has been working for the company for
 FROM employees
 WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31' AND birth_date LIKE '%-12-25'
 ORDER BY birth_date, hire_date DESC;
+
+-- Notes
+--
+-- SELECT CONCAT('Hello ', 'World', '!');
+--
+-- SELECT now();
+--
+-- SELECT CURDATE();
+--
+-- SELECT CONCAT(
+--     'Teaching people to code for ',
+--     UNIX_TIMESTAMP() - UNIX_TIMESTAMP('2014-02-04'),
+--     ' seconds'
+-- );
