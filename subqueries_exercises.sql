3da838aa0fe1f200537eb4709352abb039d01053
Tylor Pelletier <tylorpelletier@Tylors-MacBook-Pro.local>
2018-08-15 16:51:50 -0500

    finished subqueries exercises

---
+++ subqueries_exercises.sql
@@ -0,0 +1,32 @@
+USE employees;
+
+-- EXERCISE 1
+SELECT *
+FROM employees
+WHERE hire_date IN (
+  SELECT hire_date
+  FROM employees
+  WHERE emp_no = '101010'
+);
+
+-- EXERCISE 2
+SELECT title
+FROM titles
+WHERE emp_no IN (
+  SELECT emp_no
+  FROM employees
+  WHERE first_name = 'Aamod'
+)
+GROUP BY title;
+
+-- EXERCISE 3
+SELECT first_name, last_name
+FROM employees
+  JOIN titles
+    ON titles.emp_no = employees.emp_no
+WHERE employees.emp_no IN (
+  SELECT emp_no
+  FROM dept_manager
+  WHERE to_date > NOW()
+) AND titles.to_date > NOW()
+  AND employees.gender = 'F';
